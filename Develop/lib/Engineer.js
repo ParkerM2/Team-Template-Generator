dd26c0c4dcfb05621992cfe5cb0c1530f9acd43c
Parker <parkerlmanning@hotmail.com>
2020-09-18 23:52:51 -0500

    finished all constructors starting on classes

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,7 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+function Engineer(name, engineerId, github, email,) {
+    this.name = name;
+    this.engineerId = id;
+    this.email = email;
+    this.github = github;
+}
---
+++ Develop/lib/Intern.js
@@ -0,0 +1,7 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+function Intern(name, id, email, internSchool) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.internSchool = school
+}
