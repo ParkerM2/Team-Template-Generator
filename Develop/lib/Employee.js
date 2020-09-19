5930b11dfb240c20dfa52ae4c9d87c9f3dc173c5
Parker <parkerlmanning@hotmail.com>
2020-09-19 00:22:18 -0500

    added all classes/constructors and added in the exports

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,35 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, school, github, officeNumber) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.school = school;
+        this.github = github;
+        this.officeNumber = officeNumber;
+    }
+    fetchName() {
+        return this.name;
+    };
+    fetchId() {
+        return this.id;
+    };
+    fetchEmail() {
+        return this.email;
+    };
+    fetchSchool() {
+        return this.school;
+    };
+    fetchGitHub() {
+        return this.github;
+    };
+    fetchOfficeNumber() {
+        return this.officeNumber;
+    };
+    // add in employee as role**
+    fetchRole() {
+        console.log("get to work yo");
+        return ("Employee");
+    }
+};
+module.exports = Employee;
